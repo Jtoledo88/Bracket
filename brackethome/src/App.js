12910742054c313412870f3fb58e0d90dc78ab38
Jonathan Toledo <toledojon@gmail.com>
2019-05-21 20:42:29 +0000

    new views

--- brackethome/src/App.js
+++ brackethome/src/App.js
@@ -1,16 +1,10 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Home from './components/home/index';
-import Navbar from './components/share/navbar';
+
 
 function App() {
-  return (
-    <div>
-    <Navbar/>
-    <Home/>
-    </div>
-  );
+  return 
 }
 
 export default App;
